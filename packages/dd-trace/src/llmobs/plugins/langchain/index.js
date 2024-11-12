80f35338535b91f5aa2723e2f964bdbc89293ff1
Sam Brenner <sam.brenner@datadoghq.com>
2024-11-12 15:02:49 -0500

    wip

--- packages/dd-trace/src/llmobs/plugins/langchain/index.js
+++ packages/dd-trace/src/llmobs/plugins/langchain/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const log = require('../../../log')
-const { SPAN_KIND } = require('../../constants/tags')
 const LLMObsTagger = require('../../tagger')
 const LLMObsPlugin = require('../base')
 
@@ -15,6 +14,7 @@ const SUPPORTED_INTEGRATIONS = ['openai']
 const LLM_SPAN_TYPES = ['llm', 'chat_model', 'embedding']
 const LLM = 'llm'
 const WORKFLOW = 'workflow'
+const EMBEDDING = 'embedding'
 const SUPPORTED_OPERATIONS = ['llm', 'chat_model', 'embedding', 'chain']
 
 const ROLE_MAPPINGS = {
@@ -70,8 +70,10 @@ class LangChainLLMObsPlugin extends LLMObsPlugin {
         this._setMetaTagsFromChatModel(span, inputs, results, options, integrationName)
         break
       case 'llm':
+        this._setMetaTagsFromLLM(span, inputs, results)
         break
       case 'embedding':
+        this._setMetaTagsFromEmbedding(span, inputs, results)
         break
     }
   }
@@ -121,11 +123,10 @@ class LangChainLLMObsPlugin extends LLMObsPlugin {
     if (integration === 'openai' && options?.response_format) {
       // langchain-openai will call a beta client if "response_format" is passed in on the options object
       // we do not trace these calls, so this should be an llm span
-      this._tagger.changeKind(span, 'llm')
+      this._tagger.changeKind(span, LLM)
     }
     const spanKind = LLMObsTagger.getSpanKind(span)
     const isWorkflow = spanKind === 'workflow'
-    const tag = (isWorkflow ? this._tagger.tagTextIO : this._tagger.tagLLMIO).bind(this._tagger)
 
     const inputMessages = []
     if (!Array.isArray(inputs)) inputs = [inputs]
@@ -139,33 +140,180 @@ class LangChainLLMObsPlugin extends LLMObsPlugin {
     }
 
     if (this.spanHasError(span)) {
-      tag(span, inputMessages, [{ content: '' }])
+      if (isWorkflow) {
+        this._tagger.tagTextIO(span, inputMessages, [{ content: '' }])
+      } else {
+        this._tagger.tagLLMIO(span, inputMessages, [{ content: '' }])
+      }
       return
     }
 
-    // something
     const outputMessages = []
     let inputTokens = 0
     let outputTokens = 0
     let totalTokens = 0
+    let tokensSetTopLevel = false
+    const tokensPerRunId = {}
 
     if (!isWorkflow) {
       const tokens = this.checkTokenUsageChatOrLLMResult(results)
       inputTokens = tokens.inputTokens
       outputTokens = tokens.outputTokens
       totalTokens = tokens.totalTokens
+      tokensSetTopLevel = totalTokens > 0
     }
 
     for (const messageSet of results.generations) {
       for (const chatCompletion of messageSet) {
         const chatCompletionMessage = chatCompletion.message
-        // something
+        const role = this.getRole(chatCompletionMessage)
+        const content = chatCompletionMessage.text || ''
+        const toolCalls = this.extractToolCalls(chatCompletionMessage)
+        outputMessages.push({ content, role, toolCalls })
+
+        if (!isWorkflow && !tokensSetTopLevel) {
+          const { tokens, runId } = this.checkTokenUsageFromAIMessage(chatCompletionMessage)
+          if (!tokensPerRunId[runId]) {
+            tokensPerRunId[runId] = tokens
+          } else {
+            tokensPerRunId[runId].inputTokens += tokens.inputTokens
+            tokensPerRunId[runId].outputTokens += tokens.outputTokens
+            tokensPerRunId[runId].totalTokens += tokens.totalTokens
+          }
+        }
       }
+    }
+
+    if (!isWorkflow && !tokensSetTopLevel) {
+      inputTokens = Object.values(tokensPerRunId).reduce((acc, val) => acc + val.inputTokens, 0)
+      outputTokens = Object.values(tokensPerRunId).reduce((acc, val) => acc + val.outputTokens, 0)
+      totalTokens = Object.values(tokensPerRunId).reduce((acc, val) => acc + val.totalTokens, 0)
+    }
+
+    if (isWorkflow) {
+      this._tagger.tagTextIO(span, inputMessages, outputMessages)
+    } else {
+      this._tagger.tagLLMIO(span, inputMessages, outputMessages)
+      this._tagger.tagMetrics(span, {
+        inputTokens,
+        outputTokens,
+        totalTokens
+      })
+    }
+  }
+
+  _setMetaTagsFromLLM (span, inputs, results) {
+    const isWorkflow = LLMObsTagger.getSpanKind(span) === 'workflow'
+    const prompts = Array.isArray(inputs) ? inputs : [inputs]
+
+    let outputs
+    if (this.spanHasError(span)) {
+      outputs = [{ content: '' }]
+    } else {
+      outputs = results.generations.map(completion => ({ content: completion[0].text }))
+
+      if (!isWorkflow) {
+        const tokens = this.checkTokenUsageChatOrLLMResult(results)
+        this._tagger.tagMetrics(span, tokens)
+      }
+    }
+
+    if (isWorkflow) {
+      this._tagger.tagTextIO(span, prompts, outputs)
+    } else {
+      this._tagger.tagLLMIO(span, prompts, outputs)
+    }
+  }
+
+  _setMetaTagsFromEmbedding (span, inputs, results) {
+    const isWorkflow = LLMObsTagger.getSpanKind(span) === 'workflow'
+    let embeddingInput, embeddingOutput
+
+    if (isWorkflow) {
+      embeddingInput = this.formatIO(inputs)
+    } else {
+      const input = Array.isArray(inputs) ? inputs : [inputs]
+      embeddingInput = input.map(doc => ({ text: doc }))
+    }
+
+    if (this.spanHasError(span) || !results) {
+      embeddingOutput = ''
+    } else {
+      let embeddingDimensions, embeddingsCount
+      if (typeof results[0] === 'number') {
+        embeddingsCount = 1
+        embeddingDimensions = results.length
+      } else {
+        embeddingsCount = results.length
+        embeddingDimensions = results[0].length
+      }
+
+      embeddingOutput = `[${embeddingsCount} embedding(s) returned with size ${embeddingDimensions}]`
+    }
+
+    if (isWorkflow) {
+      this._tagger.tagTextIO(span, embeddingInput, embeddingOutput)
+    } else {
+      this._tagger.tagEmbeddingIO(span, embeddingInput, embeddingOutput)
     }
   }
 
   checkTokenUsageChatOrLLMResult (results) {
-    return {}
+    const llmOutput = results.llmOutput
+    const tokens = {
+      inputTokens: 0,
+      outputTokens: 0,
+      totalTokens: 0
+    }
+    if (!llmOutput) return tokens
+    const tokenUsage = llmOutput.tokenUsage || llmOutput.usageMetadata || llmOutput.usage || {}
+    if (!tokenUsage) return tokens
+
+    tokens.inputTokens = tokenUsage.promptTokens || tokenUsage.inputTokens || 0
+    tokens.outputTokens = tokenUsage.completionTokens || tokenUsage.outputTokens || 0
+    tokens.totalTokens = tokenUsage.totalTokens || tokens.inputTokens + tokens.outputTokens
+
+    return tokens
+  }
+
+  checkTokenUsageFromAIMessage (message) {
+    let usage = message.usage_metadata
+    const runId = message.run_id || message.id || ''
+    const runIdBase = runId ? runId.split('-').slice(0, -1).join('-') : ''
+
+    const responseMetadata = message.response_metadata || {}
+    usage = usage || responseMetadata.usage || responseMetadata.tokenUsage || {}
+
+    const inputTokens = usage.promptTokens || usage.inputTokens || usage.prompt_tokens || usage.input_tokens || 0
+    const outputTokens =
+      usage.completionTokens || usage.outputTokens || usage.completion_tokens || usage.output_tokens || 0
+    const totalTokens = usage.totalTokens || inputTokens + outputTokens
+
+    return {
+      tokens: {
+        inputTokens,
+        outputTokens,
+        totalTokens
+      },
+      runId: runIdBase
+    }
+  }
+
+  extractToolCalls (message) {
+    let toolCalls = message.tool_calls
+    if (!toolCalls) return []
+
+    const toolCallsInfo = []
+    if (!Array.isArray(toolCalls)) toolCalls = [toolCalls]
+    for (const toolCall of toolCalls) {
+      toolCallsInfo.push({
+        name: toolCall.name || '',
+        arguments: toolCall.args || {},
+        tool_id: toolCall.id || ''
+      })
+    }
+
+    return toolCallsInfo
   }
 
   formatIO (messages) {
@@ -204,14 +352,14 @@ class LangChainLLMObsPlugin extends LLMObsPlugin {
       const llmobsIntegration = this.getIntegrationName(type, provider)
 
       if (!this.isLLMIntegrationEnabled(llmobsIntegration)) {
-        return LLM
+        return type === 'embedding' ? EMBEDDING : LLM
       }
     }
 
     return WORKFLOW
   }
 
-  getIntegrationName (type, provider) {
+  getIntegrationName (type, provider = 'custom') {
     if (provider.startsWith(BEDROCK_PROVIDER_NAME)) {
       return 'bedrock'
     } else if (provider.startsWith(OPENAI_PROVIDER_NAME)) {
@@ -220,7 +368,7 @@ class LangChainLLMObsPlugin extends LLMObsPlugin {
       return 'anthropic'
     }
 
-    return 'custom'
+    return provider
   }
 
   isLLMIntegrationEnabled (integration) {
